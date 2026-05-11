0520644556a596a577ae0996b757e3e9d2665c90
sheladiya siddharth <242544181+siddharth-dev987@users.noreply.github.com>
2026-05-11 09:31:18 +0530

    Deploy frontend via GitHub Pages

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,7 +1,6 @@
 export const environment = {
   production: true,
-  // apiUrl: 'https://accudocs.onrender.com/api/v1',
-  apiUrl: 'http://13.233.143.174:3000/api/v1',
+  apiUrl: 'https://accudocs.onrender.com/api/v1',
   appName: 'AccuDocs',
   appVersion: '1.0.0',
 };
